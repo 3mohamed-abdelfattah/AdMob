8a1bfc0797f1dfb43477af25d40e7fa56fea6e5c
MohamedMohamed <m01126165016@icloud.com>
2024-06-24 19:17:09 +0300

    Admob Banner & Interstitial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.gms:play-services-ads:23.1.0")
+
 }
--- app/src/main/java/com/example/admob/MainActivity.kt
+++ app/src/main/java/com/example/admob/MainActivity.kt
@@ -3,20 +3,24 @@ package com.example.admob
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.admob.databinding.ActivityMainBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        loadBanner()
+    }
+
+    private fun loadBanner() {
+
+        MobileAds.initialize(this) {}
+            val adRequest = AdRequest.Builder().build()
+            binding.adView.loadAd(adRequest)
     }
 }
-
-////Integrate AdMob into your Android application using Kotlin.
