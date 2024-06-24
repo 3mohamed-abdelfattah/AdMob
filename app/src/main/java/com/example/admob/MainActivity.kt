86d3afa91dc373ef28243b1ed8add931c9c20a5b
MohamedMohamed <m01126165016@icloud.com>
2024-06-24 19:33:34 +0300

    Admob Banner & Interstitial

--- app/src/main/java/com/example/admob/MainActivity.kt
+++ app/src/main/java/com/example/admob/MainActivity.kt
@@ -1,26 +1,60 @@
 package com.example.admob
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.admob.databinding.ActivityMainBinding
 import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
+    private var mInterstitialAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         super.onCreate(savedInstanceState)
 
+        loadInterAd()
         loadBanner()
     }
 
     private fun loadBanner() {
 
         MobileAds.initialize(this) {}
-            val adRequest = AdRequest.Builder().build()
-            binding.adView.loadAd(adRequest)
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+    }
+
+    private fun loadInterAd() {
+        var adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712",
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                }
+            })
+
+        binding.btnAd.setOnClickListener {
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.show(this)
+            } else {
+                Toast.makeText(this, "The interstitial ad wasn't ready yet.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
     }
 }
